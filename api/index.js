adf9143240ce2d33228ff0106bd0a44f6d51402c
joyal7701 <joyalshah07@gmail.com>
2023-09-20 01:16:42 -0400

    complete sign up page functionality

--- api/index.js
+++ api/index.js
@@ -27,5 +27,5 @@ app.use("/api/auth", authRoutes);
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
   const message = err.message || "Internal Server Error";
-  return res.status(statusCode).join({ success: false, message, statusCode });
+  return res.status(statusCode).json({ success: false, message, statusCode });
 });
