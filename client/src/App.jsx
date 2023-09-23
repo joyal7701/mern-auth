df6af690994833205143ee82c97669bbf02e268a
joyal7701 <joyalshah07@gmail.com>
2023-09-22 22:08:33 -0400

    complete sign in page functionality

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -13,7 +13,7 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
-        <Route path="/sign-in " element={<SignIn />} />
+        <Route path="/sign-in" element={<SignIn />} />
         <Route path="/sign-up" element={<SignUp />} />
         <Route path="/profile " element={<Profile />} />
       </Routes>
